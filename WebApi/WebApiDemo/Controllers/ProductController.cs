56fb8d5e85cc4bf97616b9e4d2cd06fda657c9f3
hungtd <hungtd@hmsfirst.com>
2022-04-17 10:18:45 +0700

    Frist api demo

---
+++ WebApi/WebApiDemo/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.Model;
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/products")]
+        public IActionResult GetProductList()
+        {
+            return Ok(ProductService.GetProductsFromDatabase());
+        }
+
+    }
+}
---
+++ WebApi/WebApiDemo/Model/ProductService.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApiDemo.Model
+{
+    public static class ProductService
+    {
+        //public static List<Product> Products = new List<Product>
+        //{
+        //    new Product { Id = 1, Name = "Mazda"},
+        //    new Product { Id = 1, Name = "Honda city"},
+        //    new Product { Id = 1, Name = "Kia morning"}
+        //}; //database
+
+        public static List<Product> GetProductsFromDatabase()
+        {
+            return new List<Product>
+                {
+                    new Product { Id = 1, Name = "Mazda"},
+                    new Product { Id = 1, Name = "Honda city"},
+                    new Product { Id = 1, Name = "Kia morning"}
+                }; //database
+        }
+    }
+}
